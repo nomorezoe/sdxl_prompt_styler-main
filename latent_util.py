0d8f7486254b1fa0e4e91eda9040029f655d92b8
nomorezoe <zoewangwork@gmail.com>
2024-04-15 12:49:18 +0800

    update

--- latent_util.py
+++ latent_util.py
@@ -13,9 +13,66 @@
 # limitations under the License.
 
 import collections.abc as clabc
+from PIL.PngImagePlugin import PngInfo
+from PIL import Image
+import numpy as np
 
+import os
+import json
+from comfy.cli_args import args
 import torch
+import folder_paths
 
+class SaveImage_plus:
+    def __init__(self):
+        self.output_dir = folder_paths.get_output_directory()
+        self.type = "output"
+        self.prefix_append = ""
+        self.compress_level = 4
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": 
+                    {"images": ("IMAGE", ),
+                     "filename_prefix": ("STRING", {"default": "ComfyUI"})},
+                "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"},
+                }
+
+    RETURN_TYPES = ()
+    FUNCTION = "save_images"
+
+    OUTPUT_NODE = True
+
+    CATEGORY = "image"
+
+    def save_images(self, images, filename_prefix="ComfyUI", prompt=None, extra_pnginfo=None):
+        filename_prefix += self.prefix_append
+        full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir, images[0].shape[1], images[0].shape[0])
+        results = list()
+        for (batch_number, image) in enumerate(images):
+            i = 255. * image.cpu().numpy()
+            img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+            metadata = None
+            if not args.disable_metadata:
+                metadata = PngInfo()
+                if prompt is not None:
+                    metadata.add_text("prompt", json.dumps(prompt))
+                if extra_pnginfo is not None:
+                    for x in extra_pnginfo:
+                        metadata.add_text(x, json.dumps(extra_pnginfo[x]))
+
+            filename_with_batch_num = filename.replace("%batch_num%", str(batch_number))
+            file = f"{filename_with_batch_num}_{counter:05}_.png"
+            img.save(os.path.join(full_output_folder, file), pnginfo=metadata, compress_level=self.compress_level)
+            results.append({
+                "filename": file,
+                "subfolder": subfolder,
+                "type": self.type
+            })
+            counter += 1
+            break
+
+        return { "ui": { "images": results } }
 
 class ImageSelector:
     """
@@ -311,5 +368,6 @@ NODE_CLASS_MAPPINGS_4 = {
     "ImageSelector": ImageSelector,
     "ImageDuplicator": ImageDuplicator,
     "LatentSelector": LatentSelector,
-    "LatentDuplicator": LatentDuplicator
+    "LatentDuplicator": LatentDuplicator,
+    "SaveImage_plus": SaveImage_plus,
 }
