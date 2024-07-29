e72a66243f6b9f4a0841d5717cf1df5acdfdee8d
nomorezoe <zoewangwork@gmail.com>
2024-07-30 00:11:39 +0800

    up

--- utility_nodes.py
+++ utility_nodes.py
@@ -7,7 +7,7 @@ import random
 import requests
 from typing import List, Dict, Tuple
 
-from PIL import Image, ImageOps, ImageFilter
+from PIL import Image, ImageOps, ImageFilter, ImageSequence
 import numpy as np
 
 import folder_paths
@@ -16,6 +16,8 @@ from comfy_extras.nodes_upscale_model import ImageUpscaleWithModel
 
 from .utils import pil2tensor, tensor2pil, ensure_package, get_dict_attribute
 
+import hashlib
+import node_helpers
 
 MAX_RESOLUTION = 8192
 
@@ -1061,8 +1063,83 @@ class UtilModelMerge:
         return (m,)
 
 
+class LoadImage_EX:
+    @classmethod
+    def INPUT_TYPES(s):
+        input_dir = folder_paths.get_input_directory()
+        files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
+        return {"required":
+                    {"image": (sorted(files), {"image_upload": True})},
+                }
+
+    CATEGORY = "image"
+
+    RETURN_TYPES = ("IMAGE", "MASK")
+    FUNCTION = "load_image"
+    def load_image(self, image):
+        image_path = folder_paths.get_annotated_filepath(image)
+        
+        img = node_helpers.pillow(Image.open, image_path)
+        
+        output_images = []
+        output_masks = []
+        w, h = None, None
+
+        excluded_formats = ['MPO']
+        
+        for i in ImageSequence.Iterator(img):
+            i = node_helpers.pillow(ImageOps.exif_transpose, i)
+
+            if i.mode == 'I':
+                i = i.point(lambda i: i * (1 / 255))
+            image = i.convert("RGB")
+
+            if len(output_images) == 0:
+                w = image.size[0]
+                h = image.size[1]
+            
+            if image.size[0] != w or image.size[1] != h:
+                continue
+            
+            image = np.array(image).astype(np.float32) / 255.0
+            image = torch.from_numpy(image)[None,]
+            if 'A' in i.getbands():
+                mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
+                mask = 1. - torch.from_numpy(mask)
+            else:
+                mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
+            output_images.append(image)
+            output_masks.append(mask.unsqueeze(0))
+
+        if len(output_images) > 1 and img.format not in excluded_formats:
+            output_image = torch.cat(output_images, dim=0)
+            output_mask = torch.cat(output_masks, dim=0)
+        else:
+            output_image = output_images[0]
+            output_mask = output_masks[0]
+
+        return (output_image, output_mask)
+
+    @classmethod
+    def IS_CHANGED(s, image):
+        image_path = folder_paths.get_annotated_filepath(image)
+        m = hashlib.sha256()
+        with open(image_path, 'rb') as f:
+            m.update(f.read())
+        return m.digest().hex()
+
+    @classmethod
+    def VALIDATE_INPUTS(s, image):
+        if not folder_paths.exists_annotated_filepath(image):
+            return "Invalid image file: {}".format(image)
+
+        return True
+
+
+
 NODE_CLASS_MAPPINGS_2 = {
     "LoadImageFromUrl": UtilLoadImageFromUrl,
+    "LoadImage_Ex": LoadImage_EX,
     "LoadImageAsMaskFromUrl": UtilLoadImageAsMaskFromUrl,
     "StringToInt": UtilStringToInt,
     "StringToNumber": UtilStringToNumber,
@@ -1095,6 +1172,7 @@ NODE_CLASS_MAPPINGS_2 = {
 }
 NODE_DISPLAY_NAME_MAPPINGS_2 = {
     "LoadImageFromUrl": "Load Image From URL",
+    "LoadImage_Ex": "Load Image Ex",
     "LoadImageAsMaskFromUrl": "Load Image (as Mask) From URL",
     "StringToInt": "String to Int",
     "StringToNumber": "String to Number",
