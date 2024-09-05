5e9c2f73f30a6b38b72e1cc377c5b9f749f970b2
nomorezoe <zoewangwork@gmail.com>
2024-09-05 15:46:42 +0800

    up

--- reactor_extends.py
+++ reactor_extends.py
@@ -3,16 +3,12 @@ import os
 import cv2
 import numpy as np
 import torch
-# Get the absolute path of the parent directory of the current script
-my_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Add the My directory path to the sys.path list
-sys.path.append(my_dir)
-reactor_dir = os.path.abspath(os.path.join(my_dir, '..', 'comfyui-reactor-node'))
-sys.path.append(reactor_dir)
+sys.path.append(os.path.join(os.path.dirname(__file__), "../comfyui-reactor-node"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../comfyui-reactor-node/scripts"))
 
 from reactor_utils import tensor_to_pil
-#from scripts.reactor_faceswap import analyze_faces
+from reactor_swapper import analyze_faces
 
 class ReActorUpdateCountFace:
     @classmethod
@@ -30,8 +26,8 @@ class ReActorUpdateCountFace:
         image = tensor_to_pil(image)
         image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
         det_size=(640, 640)
-        #faces = analyze_faces(image, det_size)
-        return 4
+        faces = analyze_faces(image, det_size)
+        return (len(faces), )
      
 NODE_CLASS_MAPPINGS_5 = {
     "ReActorUpdateCountFace": ReActorUpdateCountFace
